beaa9f21a50113c4e8f19b304f61ed202b727947
lu-css <dossantossilvalucas963@gmail.com>
2024-01-27 14:54:09 -0300

    [chore] Setup xunit for unit tests

---
+++ Picpay.Unit/Startup.cs
@@ -0,0 +1,17 @@
+using Microsoft.Extensions.DependencyInjection;
+using Picpay.Adapters.Cryptographies;
+using Picpay.Unit.Mock.Providers.Cryptographys;
+using Picpay.DependencyInjection;
+
+namespace Picpay.Unit;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddTransient<ICryptographys, CryptographysMock>();
+
+        services.AddUseCases();
+    }
+}
+
---
+++ Picpay.Unit/src/Mock/Providers/CryptographysMock.cs
@@ -0,0 +1,18 @@
+using Picpay.Adapters.Cryptographies;
+
+namespace Picpay.Unit.Mock.Providers.Cryptographys;
+
+public class CryptographysMock : ICryptographys
+{
+    public string GenenateSalt()
+    => "<somesalt>";
+
+    public (string HashedPassword, string Salt) Hash(string password, string salt)
+        => (password + salt, salt);
+
+    public (string HashedPassword, string Salt) HashPassword(string password)
+    => Hash(password, GenenateSalt());
+
+    public bool VerifyPassword(string password, string storedHashedPassword, string storedSalt)
+      => storedHashedPassword == Hash(password, storedSalt).HashedPassword;
+}
